ae3a4e5538d876846ef623cb6b04aa9383b1ace1
GOLu496 <goluy1036@gmail.com>
2025-12-05 09:13:32 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'WhatsApp Call';
+    const options = {
+        body: event.data.text() || 'Incoming Video Call...',
+        icon: 'https://cdn-icons-png.flaticon.com/512/124/124034.png', // WhatsApp Icon
+        badge: 'https://cdn-icons-png.flaticon.com/512/124/124034.png',
+        vibrate: [200, 100, 200, 100, 200, 100, 200], // Phone vibration pattern
+        data: {
+            url: './index.html' // Clicking notification opens the app
+        }
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('fetch', (e) => {
-    // Basic fetch functionality
-    e.respondWith(fetch(e.request));
+self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
 });
