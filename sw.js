9cac78bd4858acd0e7395e9d2db1b6b43e70fc68
GOLu496 <goluy1036@gmail.com>
2025-12-05 14:23:52 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Basic fetch functionality
+    e.respondWith(fetch(e.request));
+});
